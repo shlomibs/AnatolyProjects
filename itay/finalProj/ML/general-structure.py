5b99aac86e02515c90d663ba809b460d3dcec9f6
IBS-BASH <s79161@nomishemer.ort.org.il>
2016-09-23 13:00:30 -0400

    ML try1

---
+++ itay/finalProj/ML/general-structure.py
@@ -0,0 +1,8 @@
+#/usr/bin/python
+import sklearn 
+import numpy as np
+import matplotlib
+
+
+
+
--- itay/finalProj/ML/try1.py
+++ itay/finalProj/ML/try1.py
@@ -1,8 +1,14 @@
 #/usr/bin/python
-import sklearn 
-import numpy as np
-import matplotlib
 
+from sklearn import datasets, svm
 
+print "hey"
 
+import matplotlib.pyplot as plt
+#import numpy as np
+print "hello"
+digits = datasets.load_digits()
 
+#print digits.data
+#print digits.target
+#print digits.images[0]
