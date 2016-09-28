404eb6aa61f68b9809b5bd206b50a4a48ba89036
IBS-BASH <s79161@nomishemer.ort.org.il>
2016-09-28 13:57:26 -0400

    tried threads..., needs to add kill switch

--- itay/finalProj/communication/lowLevelCommunicator.py
+++ itay/finalProj/communication/lowLevelCommunicator.py
@@ -1,9 +1,13 @@
 #!/usr/bin/python
+import time
+import threading
+
 
 class LowLevelCommunicator:
 	def startPortProtectionService(self, port):
 		# if self.ports is not defined create it
 		self.ports.append(port)
+
 		raise Exception("Not implemented exception")
 	def startRecievingThread(port):
 		raise Exception("Not implemented exception")
---
+++ itay/finalProj/examples/myThread.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python
+import time
+import threading
+
+def myFunc(arg1, arg2):
+	print arg1
+	time.sleep(arg2)
+
+
+if __name__ == "__main__":
+	arg1, arg2 = ">>>", 1
+	t = threading.Thread(target=myFunc, args = (arg1, arg2))
+	#t.daemon = True
+	t.start()
+	inp = raw_input("")
+	while inp != "exit":
+		inp = raw_input("")
+	#t.close()
+	print "slp for 3 sec..."
+	t2 = threading.Thread(target=time.sleep, args = (3))
+	t2.start()
+	t2.join() # wait for it
+	t2.terminate()
+
