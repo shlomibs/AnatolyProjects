2cd05e0d74187e99f635508bdde57c909c34eb19
root <root@localhost.localdomain>
2016-10-22 15:36:47 +0300

    started directory server

---
+++ itay/finalProj/directory server/directory server.py
@@ -0,0 +1,23 @@
+import upload_to_dirSer
+from socket import *
+
+class server:
+	def __init__:
+		self.s = socket(SOCK_DGRAM)
+		self.s.bind(("0.0.0.0", port))
+....
+
+
+
+
+
+def main():
+	# upload external address and port ..
+	server.run()
+	raise Exception("not implemented exception")
+
+if __name__ == "__main__":
+	main()
+
+
+
---
+++ itay/finalProj/directory server/upload to dirSer.py
@@ -0,0 +1,59 @@
+from ftplib import FTP
+import time, os
+def uploadDir(path, ftp, sp): # sp = is subprocess?
+	"""
+	upload a directory to the ftp server
+	"""
+	files = os.listdir(path)
+	os.chdir(path)
+	filelist = [] #to store all files
+	ftp.retrlines('LIST',filelist.append)    # append to list
+	for f in files:
+		if f == "upload to dirSer.py":
+			write(f + " skipped\n", sp)
+			continue
+		if os.path.isfile(path + '/' + f):
+			write("starting to upload file: " + str(f), sp)
+			fh = open(f, 'rb')
+			ftp.storbinary('STOR %s' % f, fh)
+			fh.close()
+			write("               uploaded\n", sp)
+		elif os.path.isdir(path + '/' + f):ft
+			exist = False
+			for file1 in filelist:
+				if f in file1:
+					exist = True
+			if not exist:
+				ftp.mkd(f)
+			ftp.cwd(f)
+			write("\nstarting to upload dir: " + str(f) + "\n", sp)
+			uploadDir(path + '/' + f)
+			write("dir uploaded\n\n", sp)
+	ftp.cwd('..')
+	os.chdir('..')
+
+def upload(sp): # sp = is subprocess?
+	try:
+		ftp = FTP('dirser.honor.es','u140460863','1346798255')
+		ftp.cwd("dirSer")
+		myPath = os.getcwd() 
+		uploadDir(myPath, ftp, sp) # now call the recursive function
+		print 'success'
+	except Exception as e:
+		print "connection failed!!!\nerr: " + str(e)
+	for i in range(3):
+		write(".", sp)
+		time.sleep(1)
+
+def write(string, sp): #sp = subprocess
+	if !sp: print string,
+
+if __name__ == "__main__":
+	if(len(sys.argv) > 1 && sys.argv[1] in ["subprocess","sp","SP"]):
+		upload(True)
+	else:
+		upload(False)
+
+
+
+
