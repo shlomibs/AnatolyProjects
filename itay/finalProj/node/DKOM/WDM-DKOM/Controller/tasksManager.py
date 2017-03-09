2fcf1f46ebcb1a4b563f0b74bee6c00846c6260c
itay <itay@ITAY-PC>
2017-03-09 20:29:11 +0200

    ..

--- itay/finalProj/node/DKOM/WDM-DKOM/Controller/tasksManager.py
+++ itay/finalProj/node/DKOM/WDM-DKOM/Controller/tasksManager.py
@@ -1,8 +1,21 @@
 
 class TasksManager:
+	#region constants
+	SEND_CMD = 's'
+	START_PROCESS_CMD = 'p'
+	QUERY_CMD = 'q'
+	PROCESS_ENDED_CODE = 'e'
+	PROCESS_DATA_CODE = 'd'
+	QUERY_RESPONSE_CODE = 'Q'
+	TASK_CODE = 't'
+	DISPLAY_CODE = 'D' # display to admin <=> only if the admin is connected
+	WRITE_FILE_CMD = 'w'
+	CLIENTS_LIST_CODE = 'c'
+	#endregion
+
 	def __init__(self, OutputFunc):
 		self.OutpuFunc = OutputFunc
-		self.tasks = [] # (
+		self.tasks = [] # [(taskId, ... # continue
 		self.nextTaskId = 1
 		
 	def ExecCmd(self, cmd): # command line like in cmd
